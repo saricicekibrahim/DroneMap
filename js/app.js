84a2427009b3df80249db64862993deff5a5b772
saricicek-pc <ibrahimsaricicek@gmail.com>
2017-03-29 23:58:59 +0300

    visual changes & added filter on right panel

--- js/app.js
+++ js/app.js
@@ -1,115 +1,107 @@
 var app = angular.module('droneApp', []).controller('droneCtrl',['$scope', '$http', '$sce', function($scope, $http, $sce) {
-			//https://www.googleapis.com/fusiontables/v1/query?sql=SELECT * FROM 1MIt8HjHPRe4_rifTaAwpK72vJ6c-oJdoa5c8Ryd7 where descriptor like 'Ankara%25' limit 5&key=AIzaSyCrjxOUtz0PEtDh9tDn6sjzObkC9dqQ1oA
-			var container = document.getElementById('popup');
-			var content = document.getElementById('popup-content');
-			var closer = document.getElementById('popup-closer');
-			app.config(function($sceDelegateProvider) {
-				$sceDelegateProvider.resourceUrlWhitelist([ 'self',
-					'https://www.youtube.com/**' ]);
+	var container = document.getElementById('popup');
+	var content = document.getElementById('popup-content');
+	var closer = document.getElementById('popup-closer');
+	app.config(function($sceDelegateProvider) {
+		$sceDelegateProvider.resourceUrlWhitelist([ 'self',
+			'https://www.youtube.com/**' ]);
+	});
+	
+    $scope.videoFilter = {
+            search: ''
+          };
+	
+	var overlay = new ol.Overlay(/** @type {olx.OverlayOptions} */ ({
+		element: container,
+		autoPan: true,
+		autoPanAnimation: {
+			duration: 250
+		}
+	}));
+
+	closer.onclick = function() {
+		$scope.closePopup();
+	};
+
+	$scope.closePopup = function()
+	{
+		overlay.setPosition(undefined);
+		closer.blur();
+		return false;
+	}
+	$scope.searchValue = "";
+
+	$scope.getIframeSrc = function(src) {
+		return $sce.trustAsResourceUrl("https://www.youtube.com/embed/"
+			+ src);
+	};
+	
+	$scope.map = new ol.Map({
+        interactions: ol.interaction.defaults().extend([
+            new ol.interaction.DragRotateAndZoom()
+          ]),
+		target : 'map',
+		layers : [ new ol.layer.Tile({
+			source : new ol.source.OSM()
+		})],
+		view : new ol.View({
+			center : ol.proj.transform([ 32.9, 39.9 ], 'EPSG:4326', 'EPSG:3857'),
+			zoom : 10
+		}),
+		overlays: [overlay]
+	});
+	$scope.videoLayer = null;
+	
+	$http.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT * FROM 1MIt8HjHPRe4_rifTaAwpK72vJ6c-oJdoa5c8Ryd7&key=AIzaSyCrjxOUtz0PEtDh9tDn6sjzObkC9dqQ1oA")
+	.then(function(response) {
+		$scope.fusionData = response.data.rows;
+		$scope.features = [];
+		angular.forEach($scope.fusionData, function(value, key) {
+			var latLon = value[1].split(",");
+			$scope.features.push(new ol.Feature({
+				geometry : new ol.geom.Point(
+					ol.proj.transform([
+						parseFloat(latLon[1]),
+						parseFloat(latLon[0])
+						], 'EPSG:4326','EPSG:3857')),
+				youtube_id:value[2],
+				descr:value[3]}));
+
+		});
+
+		$scope.videoMapSource = new ol.source.Vector({
+			features: $scope.features
+		});
+
+		$scope.videoLayer = new ol.layer.Vector({
+			source: $scope.videoMapSource,
+			style: new ol.style.Style({
+				image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+					src: 'img/videoIcon.png'
+				}))
+			})
+		});
+		$scope.map.addLayer($scope.videoLayer);
+		
+		$scope.map.getView().fit($scope.videoLayer.getSource().getExtent(), $scope.map.getSize());
+	});
+	
+	var element = document.getElementById('popup');
+	
+	$scope.map.on('click', function(evt) {
+		var feature = $scope.map.forEachFeatureAtPixel(evt.pixel,
+			function(feature) {
+				return feature;
 			});
+		if (feature) {
 			
-			var overlay = new ol.Overlay(/** @type {olx.OverlayOptions} */ ({
-				element: container,
-				autoPan: true,
-				autoPanAnimation: {
-					duration: 250
-				}
-			}));
+			var coordinate = evt.coordinate;
 
-			closer.onclick = function() {
-				$scope.closePopup();
-			};
-
-			$scope.closePopup = function()
-			{
-				overlay.setPosition(undefined);
-				closer.blur();
-				return false;
-			}
-			$scope.searchValue = "";
-
-			$scope.getIframeSrc = function(src) {
-				return $sce.trustAsResourceUrl("https://www.youtube.com/embed/"
-					+ src);
-			};
-
-			$scope.myFunc = function() {
-
-				$scope.fusionData = [
-
-				];
-			};
-			
-			$scope.map = new ol.Map({
-				target : 'map',
-				layers : [ new ol.layer.Tile({
-					source : new ol.source.OSM()
-				})],
-				view : new ol.View({
-					center : ol.proj.transform([ 32.9, 39.9 ], 'EPSG:4326', 'EPSG:3857'),
-					zoom : 10
-				}),
-				overlays: [overlay]
-			});
-			$scope.videoLayer = null;
-			
-			$http.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT * FROM 1MIt8HjHPRe4_rifTaAwpK72vJ6c-oJdoa5c8Ryd7 limit 5&key=AIzaSyCrjxOUtz0PEtDh9tDn6sjzObkC9dqQ1oA")
-			.then(function(response) {
-				$scope.fusionData = response.data.rows;
-				$scope.features = [];
-				angular.forEach($scope.fusionData, function(value, key) {
-					var latLon = value[1].split(",");
-					$scope.features.push(new ol.Feature({
-						geometry : new ol.geom.Point(
-							ol.proj.transform([
-								parseFloat(latLon[1]),
-								parseFloat(latLon[0])
-								], 'EPSG:4326','EPSG:3857')),
-						youtube_id:value[2],
-						descr:value[3]}));
-
-				});
-
-				$scope.videoMapSource = new ol.source.Vector({
-					features: $scope.features
-				});
-
-				$scope.videoLayer = new ol.layer.Vector({
-					source: $scope.videoMapSource,
-					style: new ol.style.Style({
-						image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-							src: 'img/videoIcon.png'
-						}))
-					})
-				});
-				$scope.map.addLayer($scope.videoLayer);
-				
-							var extent = $scope.videoLayer.getSource().getExtent();
-			$scope.map.getView().fit(extent, $scope.map.getSize());
-
-				});
-				
-
-			
-			var element = document.getElementById('popup');
-
-			$scope.map.on('click', function(evt) {
-				var feature = $scope.map.forEachFeatureAtPixel(evt.pixel,
-					function(feature) {
-						return feature;
-					});
-				if (feature) {
-					
-					var coordinate = evt.coordinate;
-
-					content.innerHTML = feature.get('descr') + "<iframe width='350' height='250' src='https://www.youtube.com/embed/" + feature.get('youtube_id') + "' frameborder='0' allowfullscreen></iframe>";
-					overlay.setPosition(coordinate);
-				}
-				else{
-					$scope.closePopup();
-				}
-			});
-
-			
-			//});
-		}]);
+			content.innerHTML = feature.get('descr') + "<iframe width='350' height='250' src='https://www.youtube.com/embed/" + feature.get('youtube_id') + "' frameborder='0' allowfullscreen></iframe>";
+			overlay.setPosition(coordinate);
+		}
+		else{
+			$scope.closePopup();
+		}
+	});
+}]);
